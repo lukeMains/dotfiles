03bc791958a9455978507c263c799baf9d1235fd
Luke M <alienmuskox@nightwing.mynetworksettings.com>
2024-05-15 22:29:37 -0400

    Create shell script for seting up dotfiles

---
+++ stow.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# TODO: heck GNU stow is installed
+
+# Alacritty
+# TODO: check .config/alacritty dir exists
+stow -t "$HOME/.config/alacritty" Alacritty
+
+# Emacs
+stow -t "$HOME" Emacs
