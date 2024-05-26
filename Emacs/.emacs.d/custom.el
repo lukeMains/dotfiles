f32d040b42d3245a001eb55b237f8b6596520914
Luke Mains <past.hat6941@fastmail.com>
2024-05-26 12:06:47 -0400

    Commit empty 'custom.el'

--- Emacs/.emacs.d/custom.el
+++ Emacs/.emacs.d/custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(sanityinc-tomorrow-bright))
- '(package-selected-packages
-   '(move-dup page-break-lines multiple-cursors symbol-overlay rainbow-delimiters unfill envrc exec-path-from-shell anzu diredfl ibuffer-vc consult-eglot git-modes blamer git-blamed diff-hl fullframe magit-todos magit pcre2el hl-todo async go-mode rust-mode auto-virtualenv toml-mode json-mode yaml-mode markdown-mode highlight-indent-guides rainbow-mode default-text-scale orderless corfu-terminal corfu consult marginalia vertico dimmer color-theme-sanityinc-tomorrow gruvbox-theme autothemer zenburn-theme jinx nyan-mode minions info-colors helpful which-key)))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
