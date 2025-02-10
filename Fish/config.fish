988d084c808004140ebc4f397ecc991eb795987e
Luke Mains <past.hat6941@fastmail.com>
2025-02-10 10:12:43 -0500

    Update fish config

--- Fish/config.fish
+++ Fish/config.fish
@@ -1,9 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    pyenv init - | source
+    if command -q pyenv
+        pyenv init - | source
+    end
 
     # fnm (fast node manager)
-    fnm env --use-on-cd | source
+    if command -q fnm
+        fnm env --use-on-cd | source
+    end
 
     # pnpm
     set -gx PNPM_HOME "/home/alienmuskox/.local/share/pnpm"
@@ -15,10 +19,3 @@ if status is-interactive
     # Add local programs to path
     set -gx PATH "$HOME/.local/bin" $PATH
 end
-
-# pnpm
-set -gx PNPM_HOME "/home/alienmuskox/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
